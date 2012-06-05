da09f272f5e1d976271dc5d824d8947ccf36f217
Paulo Neto <joaopaulocbjnp@gmail.com>
2012-06-05 16:17:47 -0300

    etl coord juv

---
+++ acao-coordenadoriajuventude/bin/migracao_juventude.pl
@@ -0,0 +1,351 @@
+use Sedna;
+use warnings;
+use lib '/home/claudio/devel/acao/Acao/lib';
+use Acao;
+use Data::Dumper;
+use strict;
+use utf8;
+use Encode;
+use Storable;
+use warnings;
+use Spreadsheet::Read;
+
+my $ref = ReadData ("FICHA.xls");
+
+my $auth = {
+          'autorizacao' => [
+                           {
+                             'principal' => 'ou=SIS,o=CTI,o=GP,o=PMF,dc=adm,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br',
+                             'role' => 'alterar'
+                           },
+                           {
+                             'principal' => 'ou=SIS,o=CTI,o=GP,o=PMF,dc=adm,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br',
+                             'role' => 'criar'
+                           },
+                           {
+                             'principal' => 'ou=SIS,o=CTI,o=GP,o=PMF,dc=adm,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br',
+                             'role' => 'listar'
+                           },
+                           {
+                             'principal' => 'ou=SIS,o=CTI,o=GP,o=PMF,dc=adm,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br',
+                             'role' => 'visualizar'
+                           },
+                           {
+                             'principal' => 'ou=SIS,o=CTI,o=GP,o=PMF,dc=adm,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br',
+                             'role' => 'transferir'
+                           }
+                         ]
+        };
+
+my $x;
+my $cell = cr2cell (2, 2);
+my $valor = $ref->[1]{$cell};
+# array onde sera atribuido os nomes e armazenado os campos contido no documento xls
+my @array = ('data', 'organizacao', 'endereco', 'bairro', 'cidade', 'cep', 'fone', 'email', 'site', 'blog', 'ano_fundacao', 
+             'cnpj', 'tp_org', 'temas', 'atividade', 'territ_atua', 'pub_alvo', 'fx_etaria', 'qt_jovens', 'local_atv', 
+             'freq_reuniao','funcao', 'resp', 'fone_resp', 'email_resp' );
+my @docs;
+my $i = 2;
+ # laço para atribuir valores ao array varrendo todo o documento xls
+while($valor ne ''){        
+	my %hash;
+    $x=1;
+    foreach my $a(@array){		
+        $cell = cr2cell ( $x, $i);
+        $valor = $ref->[1]{$cell};
+        $hash{$a} = $valor;
+        $x++;
+		$cell = cr2cell ( 1, $i);
+        $valor = $ref->[1]{$cell};
+        
+    };
+    $i++;
+    push @docs, \%hash;
+} 
+	for (my $b = 0; $b < scalar@docs; $b++)
+	{
+		
+# campo organizacao esse bloco   sera feito um tratamento nos valores para que possa ficar no padrao do formulario
+	my $org =($docs[$b]->{tp_org});
+	$org =~ s/^\s+//;
+    $org =~ s/\s+$//;
+    my $tOrg = '';
+    my $tOutroOrg = '';
+	my $ORG =lc($org);
+	my $outroOrg = "";	
+		if ($ORG  ne 'og' and 
+	        $ORG  ne 'ong'and 
+	        $ORG  ne 'associação' and
+	        $ORG  ne 'movimento'  and
+	        $ORG  ne 'sindicato'  and
+	        $ORG  ne ""){
+		          $outroOrg = $org;
+		          $org = 'Outros';
+		          $tOrg = "<tipoOrganiz>".$org."</tipoOrganiz>";
+	              $tOutroOrg = "<outrosTipoOrganiz>".$outroOrg."</outrosTipoOrganiz>";	
+			}else{
+				if ($org  eq 'OG' or 
+	                $org  eq 'ONG'  ){
+		                  $org = uc($org);
+		                  $tOrg = "<tipoOrganiz>".$org."</tipoOrganiz>";
+	                      $tOutroOrg = "";
+					}elsif ($ORG  ne ""){					      
+		                  $org = ucfirst($org);
+		                  $tOrg = "<tipoOrganiz>".$org."</tipoOrganiz>";
+	                      $tOutroOrg = "";
+				}
+		}
+		if  ($ORG  eq ""){
+		    $tOrg = "";
+	        $tOutroOrg = "";
+		    }
+
+# campo tematicas abordadas esse bloco sera feito um tratamento nos valores para que possa ficar no padrao do formulario
+	my $temas =($docs[$b]->{temas});
+	$temas =~ s/^\s+//;
+    $temas =~ s/\s+$//; 
+	my $TEMAS =lc($temas);
+	my $outroTemas = "";
+	my $tTemas = '';
+	my $tOutroTemas = '';
+		
+		if ($TEMAS  ne 'trabalho e renda'and 
+		    $TEMAS  ne 'educação' and 
+	        $TEMAS  ne 'saúde'  and 
+	        $TEMAS  ne 'gênero'  and 
+	        $TEMAS  ne 'diversidade sexual'   and
+	        $TEMAS  ne "sexualidade"  and
+	        $TEMAS  ne "acessibilidade pessoa com deficiência"  and
+	        $TEMAS  ne "ambiental"  and
+	        $TEMAS  ne "drogas"  and
+	        $TEMAS  ne "étnico-racial"  and
+	        $TEMAS  ne "religião"  and
+	        $TEMAS  ne "cultura"  and
+	        $TEMAS  ne "esporte e lazer"  and
+	        $TEMAS  ne "direitos humanos e cidadania"  and
+	        $TEMAS  ne "comunicação"  and
+	        $TEMAS  ne "" ){
+				   $outroTemas = ucfirst($temas);
+		           $temas = 'Outros';
+		           $tTemas="<tematicaAbordadas>".$temas."</tematicaAbordadas>";
+	               $tOutroTemas="<outrasTematicasAbordadas>".$outroTemas."</outrasTematicasAbordadas>";
+		}elsif($TEMAS  ne "" ){
+				   $temas = ucfirst($temas);
+		           $tTemas="<tematicaAbordadas>".$temas."</tematicaAbordadas>";
+	               $tOutroTemas="";
+			
+			
+			}		
+		
+			 if ($TEMAS  eq "" ){	
+		           $tTemas="";
+	               $tOutroTemas="";
+	               }
+		           
+		           
+# campo faixa etaria esse bloco sera feito um tratamento nos valores para que possa ficar no padrao do formulario
+	my $fxE =($docs[$b]->{fx_etaria});
+	my $outrasfxE = "";	
+	my $tFxe ='';
+	my $tOutrasfxE = "";
+	$fxE =~ s/^\s+//;
+	$fxE =~ s/\s+$//;
+	my $FXE =lc($fxE);
+	
+
+		
+		if ($FXE  ne '15 a 18 anos'and 
+	        $FXE  ne '19 a 24 anos'and
+	        $FXE  ne '25 a 29 anos' and 
+	        $FXE  ne ""){
+				   $outrasfxE= ucfirst($fxE);
+		           $fxE = 'Outros';
+		           $tFxe ="<faixaEtariaParticip>".$fxE ."</faixaEtariaParticip>";
+	               $tOutrasfxE ="<outrosParticip>".$outrasfxE."</outrosParticip>";
+		}elsif($FXE  ne ""){
+				   $outrasfxE= ucfirst($fxE);
+		           $fxE = 'Outros';
+		           $tFxe ="<faixaEtariaParticip>".$fxE ."</faixaEtariaParticip>";
+	               $tOutrasfxE =""
+			}							
+		 if ($FXE  eq ""){				  	      
+		      $tFxe ="";
+	          $tOutrasfxE ="";	           		           	           
+		           }          
+           
+# campo quantidade de jovens que partivipa da ativiades esse bloco sera feito um tratamento nos valores para que possa ficar no padrao do formulario
+	my $tegQtdJo ;
+	my $qtdJo =($docs[$b]->{qt_jovens});
+	$qtdJo =~ s/^\s+//;
+	$qtdJo =~ s/\s+$//;
+    $qtdJo =lc($qtdJo);
+	
+		
+		if ($qtdJo eq '10 a 20'and 
+	        $qtdJo eq '20 a 40'and
+	        $qtdJo eq 'mais de 40' and 
+	        $qtdJo  eq ""){
+				
+				$tegQtdJo =  "<jovensParticipAtividOrganiz>".$qtdJo."</jovensParticipAtividOrganiz> "; 
+		
+		        
+	    } else {	
+
+			    $tegQtdJo =  "";		         
+		 }      
+	      	           
+										
+# campo 4.1. Onde acontecem as atividades da Organização esse bloco sera feito um tratamento nos valores para que possa ficar no padrao do formulario
+	my $atv =($docs[$b]->{local_atv});
+	my $outroAtv = "";
+	my $tAtv='';
+	my $tOutroAtv ='';
+    $atv =~ s/^\s+//;
+    $atv =~ s/\s+$//; 	
+	my $ATV =lc($atv);	
+	
+		if ($ATV  ne 'escola'and
+		    $ATV  ne 'sede própria'and 
+	        $ATV  ne 'casa de amigos' and 
+	        $ATV  ne 'igreja' and 
+	        $ATV  ne 'universidade'  and
+	        $ATV  ne "" ){				
+				   $outroAtv  = ucfirst($atv);				   
+		           $atv = 'Outros';		
+		           $tAtv=     "<ondeAcontecemAtividOrganiz>".$atv."</ondeAcontecemAtividOrganiz>";
+	               $tOutroAtv="<outrosAtividorganiz>".$outroAtv ."</outrosAtividorganiz>"; 	
+		}elsif ($ATV ne ""){
+			     $atv = ucfirst($atv);
+		         $tAtv="<ondeAcontecemAtividOrganiz>".$atv."</ondeAcontecemAtividOrganiz>";;
+	             $tOutroAtv="";               
+		         
+		         }
+		
+		if ($ATV eq ""){
+		         $tAtv='';
+	             $tOutroAtv='';               
+		         
+		         }
+		
+# campo 4.2. Com que freqüência costuma se reunir esse bloco sera feito um tratamento nos valores para que possa ficar no padrao do formulario
+	my $freReun =($docs[$b]->{freq_reuniao});
+	$freReun =~ s/^\s+//;
+    $freReun =~ s/\s+$//;           
+	my $FREREUN =uc($freReun);
+	my $outroFreReun = "";
+		
+		if ($FREREUN  ne 'DIARIAMENTE'and
+		    $FREREUN  ne 'SEMANALMENTE'and
+	        $FREREUN  ne 'QUIZENALMENTE' and 
+	        $FREREUN  ne  'MENSALMENTE' and 
+	        $FREREUN  ne "" ){
+				   $outroFreReun  = ucfirst($freReun);
+		           $freReun = 'Outro';          
+		}								        
+				 if ($FREREUN  eq "" ){  $outroFreReun  = ' ';
+		           $freReun = 'Outro'; }
+		           
+# tratamento no campo data para adequar aos padrao do sedna e do formulario 		           
+my $dateTime = $docs[$b]->{data};
+$dateTime  =~ s/^(.*)\/(.*)\/(.*)/$3-$2-$1/go;
+
+    my $xml .= "
+<orgMovEst xmlns=\"http://schemas.fortaleza.ce.gov.br/acao/coordenadoriajuventude-cad-org_mov_est.xsd\">
+<identificOrganiz>
+	<organizacao>".$docs[$b]->{organizacao}."</organizacao>
+	<endereco>".$docs[$b]->{endereco}."</endereco>
+	<bairro>".$docs[$b]->{bairro}."</bairro>
+	<cidade>".$docs[$b]->{cidade}."</cidade>
+	<cep>".$docs[$b]->{cep}."</cep>
+	<fone>".$docs[$b]->{fone}."</fone>
+	<email>".$docs[$b]->{email}."</email>
+	<site>".$docs[$b]->{site}."</site>
+	<blog>".$docs[$b]->{blog}."</blog>
+	<anoFundacao>".$docs[$b]->{ano_fundacao}."</anoFundacao>
+	<cnpj>".$docs[$b]->{cnpj}."</cnpj>
+</identificOrganiz>
+<infoSobreOrganiz>
+    ".$tOrg."
+    ".$tOutroOrg."
+    ".$tTemas.";
+	".$tOutroTemas.";
+	<atividadesDesenvolvidas>".$docs[$b]->{atividade}."</atividadesDesenvolvidas>
+	<territorioAtuacao>".$docs[$b]->{territ_atua}."</territorioAtuacao>
+</infoSobreOrganiz>
+<participantes>
+	<publicoAlvo>".$docs[$b]->{pub_alvo}."</publicoAlvo>
+    ".$tFxe."
+	".$tOutrasfxE."
+	".$tegQtdJo."
+</participantes>
+<formaOrganiz>
+	".$tAtv."
+	".$tOutroAtv."
+	<frequenciaCostumaReunir>".$freReun."</frequenciaCostumaReunir>
+	<outrosFrenquenc>".$outroFreReun ."</outrosFrenquenc>
+	<nomeResponsavel>".$docs[$b]->{resp}."</nomeResponsavel>
+	<funcao>".$docs[$b]->{funcao}."</funcao>
+	<telefone>".$docs[$b]->{fone_resp}."</telefone>
+	<email>".$docs[$b]->{email_resp}."</email>
+	<data>".$dateTime."</data>
+</formaOrganiz>
+</orgMovEst>
+";
+ 
+# chamada dos metodos passando os valores aos atributos 
+my $controle = criaProntuario($docs[$b]->{organizacao});
+inserirDocumento( $xml, $controle);
+
+}
+
+#conexao com o banco sedna de uso
+my $sedna = Sedna->connect('127.0.0.1', 'acao', 'acao', 'acao');
+#$sedna->setConnectionAttr(AUTOCOMMIT => Sedna::SEDNA_AUTOCOMMIT_OFF() );
+
+{   package DumbUser;
+    use Moose;
+    sub memberof {[ Acao->config->{roles}{dossie}{criar}, Acao->config->{roles}{volume}{criar} , Acao->config->{roles}{documento}{criar}]}
+    sub uid {'importacao.juventude'}
+    sub id {'importacao.juventude'}
+    sub cn {'Importação.juventude'}
+}
+
+# chamada do método criaProntuario passando os respectivos paramentros 
+sub criaProntuario{
+        my($nome, $representaVolumeFisico, $classificacoes,$autorizacoes) = (shift , 0, {'classificacao' => ["cn=Juventude,dc=assuntos,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br"]}, $auth );
+        my $ip = '127.0.0.1';
+		my $localizacao = 'dc=local,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br';	
+        my $id_volume = 'volume-FE6F7D4E-AF1A-11E1-B6DD-798906AD0966';
+        my $herdar_author = '1';
+        my $dossie_fisico = 0;
+		$nome =~ tr/a-zàáâãäåæçèéêëìíîïðñòóôõöøùúûüý/A-ZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝ/;
+		#my $octets_nome = decode("utf8", $nome);
+        my $octets_nome =  $nome;
+
+        
+        my $model = Acao::Model::Dossie->new(user => DumbUser->new(), sedna => Acao->model('Sedna'), dbic => Acao->model('DB')->schema);
+        my $collection = $model->criar_dossie( {ip=>$ip, nome=>$octets_nome, id_volume =>$id_volume, dossie_fisico=>$dossie_fisico, classificacoes=>$classificacoes, 
+																							localizacao=>$localizacao, herdar_author=>$herdar_author, autorizacoes=>$autorizacoes});
+        warn "collection $collection criada com sucesso!";
+        return $collection;
+    }
+# chamada do método inserirDocumento passando os respectivos paramentros e inserindo dados de acordo com o padrao do documento 
+sub inserirDocumento{
+
+	    my($xml, $controle) = @_;
+        my ($representaVolumeFisico, $classificacoes,$autorizacoes) = ( 0, {'classificacao' => ["cn=Juventude,dc=assuntos,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br"]}, $auth );
+        my $ip = '127.0.0.1';
+        my $id_volume = 'volume-FE6F7D4E-AF1A-11E1-B6DD-798906AD0966';
+        my $xsdDocumento ='http://schemas.fortaleza.ce.gov.br/acao/coordenadoriajuventude-cad-org_mov_est.xsd';
+        my $representaDocumentoFisico = 0;
+        my $herdar_author = '1';
+		my $localizacao = 'dc=local,dc=diretorio,dc=fortaleza,dc=ce,dc=gov,dc=br';	
+        my $id_Prontuario ='$collection';
+        my $id_documento ='';       
+        my $dossie_fisico = 0;
+        my $model = Acao::Model::Documento->new(user => DumbUser->new(), sedna => Acao->model('Sedna'), dbic => Acao->model('DB')->schema);
+        my $cDoc = $model->inserir_documento( $ip, $xml, $id_volume, $controle, $xsdDocumento, $representaDocumentoFisico, $herdar_author, $autorizacoes, $id_documento);
+        warn " Documento $cDoc inserido com sucesso!";                                
+                               
+ }
+
--- acao-habitafor/bin/migracao_habitafor.pl
+++ acao-habitafor/bin/migracao_habitafor.pl
@@ -7,23 +7,15 @@ use strict;
 use utf8;
 use Storable;
 
-
-
-local $/ = undef;
-open(FILE, "<:encoding(UTF-8)", "../consultas/migracao_vm.txt");
-my $xquery = <FILE>;
-close FILE;
-
 my $sedna = Sedna->connect('127.0.0.1', 'AcaoDb', 'acao', '12345');
-#my $sedna = Sedna->connect('172.30.116.73', 'acao', 'acao', '12345');
 $sedna->setConnectionAttr(AUTOCOMMIT => Sedna::SEDNA_AUTOCOMMIT_OFF() );
 
 {   package DumbUser;
     use Moose;
     sub memberof {[ Acao->config->{roles}{dossie}{criar}, Acao->config->{roles}{volume}{criar} , Acao->config->{roles}{documento}{criar}]}
-    sub uid {'importacao.vila.do.mar'}
-    sub id {'importacao.vila.do.mar'}
-    sub cn {'Importação vila.do.mar'}
+    sub uid {'importacao.habitafor'}
+    sub id {'importacao.habitafor'}
+    sub cn {'Importação habitafor'}
 }
 
 
@@ -52,157 +44,6 @@ my $auth = {
                          ]
         };
 
-sub init{
-	my $id_volume = criaVolume();
-	my %documentos;
-
-	$sedna->begin;
-	$sedna->execute($xquery);
-
-	my $i = 1;
-	while ($sedna->next){
-		warn "consultando... $i" unless  $i%100;
-		my $controle = $sedna->getItem();
-		$controle =~ s/\D//go;
-		$sedna->next;
-		$documentos{$controle} = $sedna->getItem();
-		$i++;
-	}
-
-	$sedna->commit;
-
-        store \%documentos, 'documentos.st';
-#for \$z in collection("leitura-1")[cc:registroDigitacao/cc:documento/cc:controle/text()="$ctrl" and cc:registroDigitacao/cc:documento/cc:estado/text() eq "Rejeitado"]
-	my $counter = 0;
-	for my $ctrl (keys %documentos) {
-		$counter++;
-		warn "preparando... $counter" unless  $counter%100;
-#consulta todos os forms caderno A com estado rejeitado
-		my $xq = qq|declare namespace cc = "http://schemas.fortaleza.ce.gov.br/acao/controledigitacao.xsd";
-declare namespace vc = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-consolidado.xsd";
-declare namespace fa = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-cadernoa.xsd";
-declare namespace fb = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-cadernob.xsd";
-declare namespace ns="http://schemas.fortaleza.ce.gov.br/acao/dossie.xsd";
-declare namespace dc="http://schemas.fortaleza.ce.gov.br/acao/documento.xsd";
-declare namespace audt="http://schemas.fortaleza.ce.gov.br/acao/auditoria.xsd";
-declare namespace class="http://schemas.fortaleza.ce.gov.br/acao/classificacao.xsd";
-
-for \$z in index-scan("controle1", "$ctrl", 'EQ')[cc:documento/cc:estado/text() eq "Rejeitado"]
-return 
-          <documento xmlns="http://schemas.fortaleza.ce.gov.br/acao/documento.xsd">
-              <id>{\$z/cc:documento/cc:controle/text()}</id>
-              <nome/>
-              <criacao>{\$z/cc:digitacao/cc:dataDigitacao/text()}</criacao>
-              <invalidacao>{\$z/cc:digitacao/cc:dataDigitacao/text()}</invalidacao>
-              <motivoInvalidacao/>
-              <representaDocumentoFisico>1</representaDocumentoFisico>
-              <autorizacoes herdar="1"/>
-              <documento>
-                <conteudo>
-                     { \$z/cc:documento/cc:conteudo/* }
-                </conteudo>
-              </documento>
-           </documento>  
-|;
-
-		$sedna->begin;
-		$sedna->execute($xq);
-		my $inv = '';
-		while ($sedna->next){
-			$inv .= $sedna->getItem();
-		}
-		$sedna->commit;
-		$documentos{$ctrl} =~ s/<invalidados\/>/$inv/o;
-
-#for \$y in collection("leitura-2")[cc:registroDigitacao/cc:documento/cc:conteudo/fb:formCadernoB/fb:composicaoFamiliar/fb:formularioPrincipal/text() eq "$ctrl"]
-
-		$xq = qq|declare namespace cc = "http://schemas.fortaleza.ce.gov.br/acao/controledigitacao.xsd";
-declare namespace vc = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-consolidado.xsd";
-declare namespace fa = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-cadernoa.xsd";
-declare namespace fb = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-cadernob.xsd";
-declare namespace ns="http://schemas.fortaleza.ce.gov.br/acao/dossie.xsd";
-declare namespace dc="http://schemas.fortaleza.ce.gov.br/acao/documento.xsd";
-declare namespace audt="http://schemas.fortaleza.ce.gov.br/acao/auditoria.xsd";
-declare namespace class="http://schemas.fortaleza.ce.gov.br/acao/classificacao.xsd";
-
-for \$y in index-scan("formularioPrincipal", "$ctrl", 'EQ')[cc:documento/cc:estado/text() eq "Aprovado"]
-return
-          <documento xmlns="http://schemas.fortaleza.ce.gov.br/acao/documento.xsd">
-              <id>{\$y/cc:documento/cc:controle/text()}</id>
-              <nome/>
-              <criacao>{\$y/cc:digitacao/cc:dataDigitacao/text()}</criacao>
-              <invalidacao>1970-01-01T00:00:00Z</invalidacao>
-              <motivoInvalidacao/>
-              <representaDocumentoFisico>1</representaDocumentoFisico>
-              <autorizacoes herdar="1"/>
-              <documento>
-                <conteudo>
-                     { \$y/cc:documento/cc:conteudo/* }
-                </conteudo>
-              </documento>
-           </documento>
-
-|;
-
-		$sedna->begin;
-		$sedna->execute($xq);
-		my $cdb = '';
-		while ($sedna->next){
-			$cdb.= $sedna->getItem();
-		}
-		$sedna->commit;
-
-		$xq = qq|declare namespace cc = "http://schemas.fortaleza.ce.gov.br/acao/controledigitacao.xsd";
-declare namespace vc = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-consolidado.xsd";
-declare namespace fa = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-cadernoa.xsd";
-declare namespace fb = "http://schemas.fortaleza.ce.gov.br/habitafor/viladomar-cadernob.xsd";
-declare namespace ns="http://schemas.fortaleza.ce.gov.br/acao/dossie.xsd";
-declare namespace dc="http://schemas.fortaleza.ce.gov.br/acao/documento.xsd";
-declare namespace audt="http://schemas.fortaleza.ce.gov.br/acao/auditoria.xsd";
-declare namespace class="http://schemas.fortaleza.ce.gov.br/acao/classificacao.xsd";
-
-for \$y in index-scan("formularioPrincipal", "$ctrl", 'EQ')[cc:documento/cc:estado eq "Rejeitado"]
-return
-          <documento xmlns="http://schemas.fortaleza.ce.gov.br/acao/documento.xsd">
-              <id>{\$y/cc:documento/cc:controle/text()}</id>
-              <nome/>
-              <criacao>{\$y/cc:digitacao/cc:dataDigitacao/text()}</criacao>
-              <invalidacao>{\$y/cc:digitacao/cc:dataDigitacao/text()}</invalidacao>
-              <motivoInvalidacao/>
-              <representaDocumentoFisico>1</representaDocumentoFisico>
-              <autorizacoes herdar="1"/>
-              <documento>
-                <conteudo>
-                     { \$y/cc:documento/cc:conteudo/* }
-                </conteudo>
-              </documento>
-           </documento>
-
-|;
-
-		$sedna->begin;
-		$sedna->execute($xq);
-		while ($sedna->next){
-			$cdb.= $sedna->getItem();
-		}
-		$sedna->commit;
-
-		$documentos{$ctrl} =~ s/<cadernob\/>/$cdb/o;
-
-
-	}
-
-#	print &Dumper(\%documentos);exit;
-	$sedna->begin;
-
-	for my $ctrl (keys %documentos) {
-		$sedna->loadData( $documentos{$ctrl}, $ctrl, $id_volume );
-		$sedna->endLoadData();
-		warn "Protuario $ctrl inserido com sucesso!";
-	}
-
-	$sedna->commit;
-}
 
 sub criaVolume{
     my $xq = 'declare namespace vol = "http://schemas.fortaleza.ce.gov.br/acao/volume.xsd"; for $x in collection("volume")/vol:volume[vol:nome/text() eq "VILA DO MAR"]/vol:collection/text() return $x';
@@ -221,40 +62,3 @@ sub criaVolume{
     }
     return $collection;
 }
-
-init();
-
-
-__END__
-     { for $z in $fainv return 
-          <documento xmlns="http://schemas.fortaleza.ce.gov.br/acao/documento.xsd">
-              <id>{$z/cc:registroDigitacao/cc:documento/cc:controle/text()}</id>
-              <nome/>
-              <criacao>{$z/cc:registroDigitacao/cc:digitacao/cc:dataDigitacao/text()}</criacao>
-              <invalidacao>{$z/cc:registroDigitacao/cc:digitacao/cc:dataDigitacao/text()}</invalidacao>
-              <motivoInvalidacao/>
-              <representaDocumentoFisico>1</representaDocumentoFisico>
-              <autorizacao principal="paulo.neto" role="role" dataIni="2011-02-01T15:08:25" dataFim=""/>
-              <documento>
-                <conteudo>
-                     { $z/cc:registroDigitacao/cc:documento/cc:conteudo/* }
-                </conteudo>
-              </documento>
-           </documento>     },
-     { for $y in $fb return
-          <documento xmlns="http://schemas.fortaleza.ce.gov.br/acao/documento.xsd">
-              <id>{$y/cc:registroDigitacao/cc:documento/cc:controle/text()}</id>
-              <nome/>
-              <criacao>{$y/cc:registroDigitacao/cc:digitacao/cc:dataDigitacao/text()}</criacao>
-              <invalidacao>1970-01-01T00:00:00Z</invalidacao>
-              <motivoInvalidacao/>
-              <representaDocumentoFisico>1</representaDocumentoFisico>
-              <autorizacoes herdar="1"/>
-              <documento>
-                <conteudo>
-                     { $y/cc:registroDigitacao/cc:documento/cc:conteudo/* }
-                </conteudo>
-              </documento>
-           </documento>
-     }
-
