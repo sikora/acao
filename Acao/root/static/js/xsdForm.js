37a85a1f02eb2d170a0f12fb77df6773af5db2ed
Arthur Molina <artz@edilson-desktop.(none)>
2012-06-26 14:18:44 -0300

    correção de bug xsdForm

--- Acao/root/static/js/xsdForm.js
+++ Acao/root/static/js/xsdForm.js
@@ -755,8 +755,7 @@ function generateForm(xsdFile,containerId) {
         var tagRaiz  = xml.getElementsByTagNameNS('http://www.w3.org/2001/XMLSchema','schema')[0];
         var elemRoot = getNodeByTagName(tagRaiz, 'xs:element'); // elemento raiz
         var elHtml = generateFormFromNode(tagRaiz, elemRoot, "xsdform___");
-        getById(containerId).appendChild(
- elHtml );
+        getById(containerId).appendChild(elHtml );
         
         var divx = document.createElement('div');
         divx.id = "requireditems";
@@ -765,6 +764,9 @@ function generateForm(xsdFile,containerId) {
         $('input[need], select[need], textarea[need]').blur( function(){onBlurVerify( this );} );
         $('input[need], select[need], textarea[need]').change( function(){returnRequiredItems("requireditems");} );
         returnRequiredItems("requireditems");
+        $(document).ready(function() {
+            returnRequiredItems("requireditems");
+         });
 
     } catch (myError) {
         alert( myError.name + ': ' + myError.message + "\n" + myError);
